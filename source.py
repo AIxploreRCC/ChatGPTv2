e1e0ffcee5c78ff499fe71a19f1a6cf907b7f483
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2023-04-29 21:23:32 +0200

    Update source.py

--- source.py
+++ source.py
@@ -10,6 +10,20 @@ from llama_index import LLMPredictor, GPTSimpleVectorIndex, PromptHelper, Servic
 from langchain import OpenAI
 
 
+# This example uses text-davinci-003 by default; feel free to change if desired
+llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003"))
 
+# Configure prompt parameters and initialise helper
+max_input_size = 4096
+num_output = 256
+max_chunk_overlap = 20
+
+prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
+
+# Load documents from the 'data' directory
+documents = SimpleDirectoryReader('data').load_data()
+index = GPTSimpleVectorIndex(
+    documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper
+)
    
 
