44ce6853d31569daaea1689e8c33329f8cee76f8
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2023-04-29 16:34:21 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -7,3 +7,10 @@ Hello *world!*
 
 
 index_file = 'index.json'
+
+
+#query the selected index
+    query = st.text_input('Enter Your Query')
+    button = st.button(f'Response')
+    if button:
+        st.write(index.query(query))
