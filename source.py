978385bf6129d330ea9be18dd1d026ceac07bc38
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2023-04-29 17:04:03 +0200

    Update source.py

--- source.py
+++ source.py
@@ -11,3 +11,15 @@ from langchain import OpenAI
 
 doc_path = 'EAU-Guidelines-on-Renal-Cell-Carcinoma-2023.pdf'
 index_file = 'index.json'
+
+if 'response' not in st.session_state:
+    st.session_state.response = ''
+
+def send_click():
+    st.session_state.response  = index.query(st.session_state.prompt)
+
+index = None
+st.title("Yeyu's Doc Chatbot")
+
+sidebar_placeholder = st.sidebar.container()
+uploaded_file = st.file_uploader("Choose a file")
