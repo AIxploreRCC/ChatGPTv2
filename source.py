80f4fea3418bdec4d957b9dbb3c52e1c7e296254
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2023-04-29 21:32:25 +0200

    Update source.py

--- source.py
+++ source.py
@@ -15,3 +15,17 @@ from llama_index import SimpleDirectoryReader
 SimpleDirectoryReader = download_loader("SimpleDirectoryReader")
 loader = SimpleDirectoryReader('Test', recursive=True, exclude_hidden=True)
 documents = loader.load_data()
+
+# define LLM
+llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003"))
+
+max_input_size = 4096
+num_output = 256
+max_chunk_overlap = 20
+prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
+
+service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+
+index = GPTSimpleVectorIndex.from_documents(
+    documents, service_context=service_context
+)
