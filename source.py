0be311b12df6cebc5ac919d9dd28642325aaf904
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2023-04-29 17:01:33 +0200

    Update source.py

--- source.py
+++ source.py
@@ -2,3 +2,12 @@ import os
 import openai
 import streamlit as st
 openai.api_key = st.secrets['key']
+
+from llama_index import download_loader
+from llama_index.node_parser import SimpleNodeParser
+from llama_index import GPTSimpleVectorIndex
+from llama_index import LLMPredictor, GPTSimpleVectorIndex, PromptHelper, ServiceContext
+from langchain import OpenAI
+
+doc_path = './data/'
+index_file = 'index.json'
