3f7c2de8f5b4796496396adf3650778beec240b4
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2023-04-29 20:58:15 +0200

    Update source.py

--- source.py
+++ source.py
@@ -9,13 +9,6 @@ from llama_index import GPTSimpleVectorIndex
 from llama_index import LLMPredictor, GPTSimpleVectorIndex, PromptHelper, ServiceContext
 from langchain import OpenAI
 
-
-index_file = 'index.json'
-
-@st.cache_resource
-def load_indexes():
-    """load the pipeline object for preprocessing and the ml model"""
-
 # load index files
 
 index_file = load('index.json')
