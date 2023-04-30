5bbac129cbd9971be2e596412be548796aff2f06
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2023-04-30 20:50:36 +0200

    Update source2.py

--- source2.py
+++ source2.py
@@ -29,19 +29,6 @@ documents = loader.load_data()
 # define LLM
 llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003"))
 
-max_input_size = 4096
-num_output = 256
-max_chunk_overlap = 20
-prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
-
-service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-
-index = GPTSimpleVectorIndex.from_documents(
-    documents, service_context=service_context
-)
-
-# Save your index to a index.json file
-index.save_to_disk('index.json')
 # Load the index from your saved index.json file
 index = GPTSimpleVectorIndex.load_from_disk('index.json', llm_predictor=llm_predictor)
 
