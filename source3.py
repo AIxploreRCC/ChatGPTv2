449e8c3a62207624ed6c0de2999448ba54b971c6
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2023-05-15 17:21:32 +0200

    Update source3.py

--- source3.py
+++ source3.py
@@ -45,6 +45,32 @@ index = GPTSimpleVectorIndex.from_documents(
 st.title("Ask Uro_GPT")
 query = st.text_input("What would you like to ask?", "")
 
-if st.button("Submit"):
-    response = index.query(query)
-    st.write(response)
+def list_bots():
+    bot_files = os.listdir("bots")
+    bots = [os.path.splitext(bot)[0] for bot in bot_files if bot.endswith('.py')]
+    return bots
+
+
+def choose_bot():
+    bots = list_bots()
+    print("Available bots:")
+    for index, bot in enumerate(bots, 1):
+        print(f"{index}. {bot}")
+
+    choice = int(input("Choose a bot by entering its number: ")) - 1
+    return bots[choice]
+
+
+def select_bot():
+    chosen_bot = choose_bot()
+    bot_module = importlib.import_module(f"bots.{chosen_bot}")
+    bot_module.main()
+
+
+if __name__ == "__main__":
+    select_bot()
+    
+st.title("Ask Zino_GPT")
+main = st.text_input("What would you like to ask?", "")
+
+
